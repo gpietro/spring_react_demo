384ecdcf1e1f46b83eab69c84de1a9671a39583a
Pietro <pietroghezzi.ch@gmail.com>
2017-11-21 22:33:30 +0100

    init

---
+++ src/main/java/react/backend/App.java
@@ -0,0 +1,20 @@
+package react.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
+
+}
---
+++ src/main/java/react/backend/MainController.java
@@ -0,0 +1,15 @@
+package react.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String index() { 
+		System.out.println("here");	
+		return "index"; 
+	}
+}
